7721dfc3f9a21b8ab6451fe29137f24c549f1b04
Rajdeep <rajdeepmallick999@gmail.com>
2023-08-02 15:44:50 +0530

    config file info update

--- index.ts
+++ index.ts
@@ -8,3 +8,15 @@ function fun(a:number,b:number):number{
 }
 
 console.log(fun(5,34))
+
+
+// whether there is any error or not, tsc will make the compiled js file
+// TS-configuration file will not let it to make the js file until remove all error
+// tsc --init
+// search for "emitonerror" , mark it true and uncomment
+
+function sum(a:number,b:number):number{
+    return a + b
+}
+
+// console.log(sum(5,"Rajdeep"))   // this will not let us make the js file
